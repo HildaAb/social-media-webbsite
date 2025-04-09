da3352f29515379fb37420e242e423b621e960ce
HildaAb <ilona.aase@gmail.com>
2025-04-09 22:04:42 +0200

    Fixed theme colors

--- src/app.js
+++ src/app.js
@@ -122,3 +122,22 @@ fontSizes.forEach((size) => {
 });
 
 //colors
+colorPalette.forEach((color) => {
+  color.addEventListener("click", () => {
+    let primary;
+
+    if (color.classList.contains("color-1")) {
+      primaryHue = 252;
+    } else if (color.classList.contains("color-2")) {
+      primaryHue = 52;
+    } else if (color.classList.contains("color-3")) {
+      primaryHue = 352;
+    } else if (color.classList.contains("color-4")) {
+      primaryHue = 152;
+    } else if (color.classList.contains("color-5")) {
+      primaryHue = 202;
+    }
+
+    root.style.setProperty("--primary-color-hue", primaryHue);
+  });
+});
