15f931a038cb860203de52b3ca28a1276aa5b80a
HildaAb <ilona.aase@gmail.com>
2025-04-09 17:04:40 +0200

    fixed the notifications section

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 //sidebar notification ,fundera varför det inte fungerar
 const menuItems = document.querySelectorAll(".menu-item");
-const popup = document.querySelector(".notifications-popup");
+const popup = document.querySelector("#notifications-popup");
 
 const messagesNotification = document.querySelector("#messages-notifications");
 
@@ -8,7 +8,15 @@ const messages = document.querySelector(".messages");
 const message = document.querySelectorAll(".message");
 const messageSearch = document.querySelector("#message-search");
 
-//notifications, fundera.Allt försvinner
+const theme = document.querySelector("#theme");
+const themeModal = document.querySelector(".customize-theme");
+
+const fontSizes = document.querySelectorAll(".choose-size span");
+let root = document.querySelector(":root");
+
+const colorPalette = document.querySelectorAll(".choose-color span");
+
+//notifications
 const changeActiveItem = () => {
   menuItems.forEach((item) => {
     item.classList.remove("active");
@@ -31,14 +39,87 @@ menuItems.forEach((item) => {
   });
 });
 
-//sidebar messages. fundera varför det inte fungerar
-
 //messages Fundera,borde fungera
 messagesNotification.addEventListener("click", () => {
-  messages.style.color = "blue";
-  messagesNotification.querySelector(".notification-count").style.display =
-    "none";
+  messages.style.color = red;
+  messages.style.backgroundColor = "green";
+
+  const messageNotification = document.querySelector(".notification-count");
+  messageNotification.style.display = "none";
   setTimeout(() => {
-    messages.style.boxShadow = "none";
-  }, 2000);
+    message.style.boxShadow = "none";
+  }, 5000);
 });
+
+//messageSearch (right). fundera varför det inte fungerar
+
+const searchMessage = () => {
+  const val = messageSearch.value;
+  message.forEach((user) => {
+    const userName = user.querySelector("h5");
+    if (userName.indexOf(val) != -1) {
+      user.style.display = "flex";
+    } else {
+      user.style.display = "none";
+    }
+  });
+};
+
+messageSearch - addEventListener("keyup", searchMessage);
+
+//theme
+
+const openThemeModal = () => {
+  themeModal.style.display = "grid";
+};
+
+const closeThemeModal = (e) => {
+  if (e.target.classList.contains("customize-theme")) {
+    themeModal.style.display = "none";
+  }
+};
+
+themeModal.addEventListener("click", closeThemeModal);
+
+theme.addEventListener("click", openThemeModal);
+//remove active from span
+
+const removeSizeSelector = () => {
+  fontSizes.forEach((size) => {
+    size.classList.remove("acitve");
+  });
+};
+
+//font-size
+fontSizes.forEach((size) => {
+  size.addEventListener("click", () => {
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle("active");
+
+    if (size.classList.containes("font-size-1")) {
+      fontSize = "10px";
+      root.style.setProperty("----sticky-top-left", "5.4rem");
+      root.style.setProperty("----sticky-top-right", "5.4rem");
+    } else if (size.classList.containes("font-size-2")) {
+      fontSize = "13px";
+      root.style.setProperty("----sticky-top-left", "5.4rem");
+      root.style.setProperty("----sticky-top-right", "-7rem");
+    } else if (size.classList.containes("font-size-3")) {
+      fontSize = "16px";
+      root.style.setProperty("----sticky-top-left", "-2rem");
+      root.style.setProperty("----sticky-top-right", "-17rem");
+    } else if (size.classList.containes("font-size-4")) {
+      fontSize = "19px";
+      root.style.setProperty("----sticky-top-left", "-5rem");
+      root.style.setProperty("----sticky-top-right", "-25rem");
+    } else if (size.classList.containes("font-size-5")) {
+      fontSize = "22px";
+      root.style.setProperty("----sticky-top-left", "-12rem");
+      root.style.setProperty("----sticky-top-right", "-35rem");
+    }
+    document.querySelector("html").style.fontSIze = fontSize;
+  });
+});
+
+//colors
