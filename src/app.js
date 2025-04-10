c35a2b029f3dd19901c9e153e3dc0c04c5f68b75
HildaAb <ilona.aase@gmail.com>
2025-04-10 11:13:41 +0200

    fixed theme background

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ let root = document.querySelector(":root");
 
 const colorPalette = document.querySelectorAll(".choose-color span");
 
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
+
 //notifications
 const changeActiveItem = () => {
   menuItems.forEach((item) => {
@@ -148,4 +152,47 @@ colorPalette.forEach((color) => {
     color.classList.add("active");
     root.style.setProperty("--primary-color-hue", primaryHue);
   });
+});
+
+//background color Fundera
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () => {
+  root.style.setProperty("--light-color-lightness", lightColorLightness);
+  root.style.setProperty("--white-color-lightness", whiteColorLightness);
+  root.style.setProperty("--dark-color-lightness", darkColorLightness);
+};
+
+Bg1.addEventListener("click", () => {
+  Bg1.classList.add("active");
+
+  Bg2.classList.remove("active");
+  Bg3.classList.remove("active");
+  window.location.reload();
+});
+
+Bg2.addEventListener("click", () => {
+  arkColorLightness = "95%";
+  whiteColorLightness = "20%";
+  lightColorLightness = "15%";
+
+  Bg2.classList.add("active");
+
+  Bg1.classList.remove("active");
+  Bg3.classList.remove("active");
+  changeBG();
+});
+
+Bg3.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  whiteColorLightness = "10%";
+  lightColorLightness = "0%";
+
+  Bg3.classList.add("active");
+
+  Bg1.classList.remove("active");
+  Bg2.classList.remove("active");
+  changeBG();
 });
